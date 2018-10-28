d1b37a145bff2a08f7bb9d7cf83efb012256c095
Harrison Burr <harrison.burr@uconn.edu>
2018-10-28 19:25:22 -0400

    changed background color of placeholder image to fit dark theme

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 var firstImg = document.querySelector('.welcomeImg');
 
-firstImg.setAttribute('src', 'https://via.placeholder.com/' + firstImg.width + 'x' + firstImg.height);
+firstImg.setAttribute('src', 'https://via.placeholder.com/' + firstImg.width + 'x' + firstImg.height + '/333');
