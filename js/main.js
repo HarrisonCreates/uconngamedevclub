d5b5b38c9d170db27bdf601df3853b5d4347c85b
Harrison Burr <harrison.burr@uconn.edu>
2018-10-28 21:26:08 -0400

    Introducing slide-in menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 var docBody = document.querySelector('.body');
-
+var hamburger = document.querySelector('.hamburgerMenu');
 var firstImg = document.querySelector('.welcomeImg');
+var slideMenu = document.querySelector('.slideInMenu');
+var closeButton = document.querySelector('.closeButton');
 
 let bodyHeight = docBody.offsetHeight;
 
 let bodyWidth = docBody.offsetWidth;
 
 firstImg.setAttribute('src', 'https://via.placeholder.com/' + firstImg.width + 'x' + firstImg.height + '/333/888');
+
+hamburger.addEventListener('click', function(){
+  slideMenu.style.left = "0%";
+});
+
+closeButton.addEventListener('click', function(){
+  slideMenu.style.left = "-100%";
+});
